c4c6bde2bbe27b272b04453b17eb89f7eba8dc43
joshuamchavez2 <joshua.m.chavez2@gmail.com>
2021-09-24 22:53:18 -0500

    added more functions

--- functions.py
+++ functions.py
@@ -1,11 +1,26 @@
 import pandas as pd
 import numpy as np
 import os
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import mean_squared_error
 
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
+from sklearn.preprocessing import PolynomialFeatures
 
+import sklearn.preprocessing
 
 def nulls_by_col(df):
     num_missing = df.isnull().sum()
@@ -13,9 +28,7 @@ def nulls_by_col(df):
     prcnt_miss = num_missing / rows * 100
     cols_missing = pd.DataFrame({'num_rows_missing': num_missing, 'percent_rows_missing': prcnt_miss})
     return cols_missing
-
-
-
+    
 def nulls_by_row(df):
     num_missing = df.isnull().sum(axis=1)
     prcnt_miss = num_missing / df.shape[1] * 100
@@ -24,9 +37,7 @@ def nulls_by_row(df):
     .groupby(['num_cols_missing', 'percent_cols_missing']).count()\
     .rename(index=str, columns={'customer_id': 'num_rows'}).reset_index()
     return rows_missing
-
-
-
+    
 def handle_missing_values(df, prop_required_columns=0.6, prop_required_row=0.75):
     threshold = int(round(prop_required_columns * len(df.index), 0))
     df = df.dropna(axis=1, thresh=threshold)
@@ -34,13 +45,9 @@ def handle_missing_values(df, prop_required_columns=0.6, prop_required_row=0.75)
     df = df.dropna(axis=0, thresh=threshold)
     return df
 
-
-
 def remove_columns(df, cols_to_remove):
     df = df.drop(columns=cols_to_remove)
     return df
-
-
 
 def add_scaled_columns(train, validate, test, scaler, columns_to_scale):
     
@@ -69,7 +76,7 @@ def add_scaled_columns(train, validate, test, scaler, columns_to_scale):
     
     return train, validate, test
 
-
+############################## Handle Outliers ###################################
 
 def remove_outliers(df, k, col_list):
     ''' remove outliers from a list of columns in a dataframe 
@@ -90,3 +97,297 @@ def remove_outliers(df, k, col_list):
         df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
         
     return df
+
+
+############################ Feature Engineering ############################
+
+def select_rfe(X, y, k):
+    # make the thing
+    lm = sklearn.linear_model.LinearRegression()
+    rfe = sklearn.feature_selection.RFE(lm, n_features_to_select=k)
+
+    # Fit the thing
+    rfe.fit(X, y)
+    
+    # use the thing
+    features_to_use = X.columns[rfe.support_].tolist()
+    
+    # we need to send show_feature_rankings a trained/fit RFE object
+    all_rankings = show_features_rankings(X, rfe)
+    
+    return features_to_use, all_rankings
+
+def show_features_rankings(X_train, rfe):
+    """
+    Takes in a dataframe and a fit RFE object in order to output the rank of all features
+    """
+    # rfe here is reference rfe from cell 15
+    var_ranks = rfe.ranking_
+    var_names = X_train.columns.tolist()
+    ranks = pd.DataFrame({'Var': var_names, 'Rank': var_ranks})
+    ranks = ranks.sort_values(by="Rank", ascending=True)
+    return ranks
+
+
+########################## Regression Models ###########################
+
+def make_metric_df(y, y_pred, model_name, metric_df):
+    if metric_df.size ==0:
+        metric_df = pd.DataFrame(data=[
+            {
+                'model': model_name, 
+                'RMSE_validate': mean_squared_error(
+                    y,
+                    y_pred) ** .5,
+                'r^2_validate': explained_variance_score(
+                    y,
+                    y_pred)
+            }])
+        return metric_df
+    else:
+        return metric_df.append(
+            {
+                'model': model_name, 
+                'RMSE_validate': mean_squared_error(
+                    y,
+                    y_pred) ** .5,
+                'r^2_validate': explained_variance_score(
+                    y,
+                    y_pred)
+            }, ignore_index=True)
+
+def baseline(y_train, y_validate, metric_df, target):
+    mean = y_train.tax_value.mean() # Train Mean
+    y_train[target] = mean
+    y_validate[target] = mean
+    
+    # make our first entry into the metric_df with median baseline
+    metric_df = make_metric_df(y_validate.tax_value,
+                           y_validate.tax_value_pred_mean,
+                           'mean_baseline',
+                          metric_df)
+    return metric_df
+
+
+def ols(X_train, y_train, X_validate, y_validate, metric_df):
+
+    lm = LinearRegression(normalize=True)
+    lm.fit(X_train, y_train.tax_value)
+    y_train['tax_value_pred_lm'] = lm.predict(X_train)
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.tax_value_pred_lm) ** (1/2)
+
+    # predict validate
+    y_validate['tax_value_pred_lm'] = lm.predict(X_validate)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.tax_value_pred_lm) ** (1/2)
+    
+    metric_df = metric_df.append({
+    'model': 'OLS Regressor', 
+    'RMSE_validate': rmse_validate,
+    'r^2_validate': explained_variance_score(y_validate.tax_value, y_validate.tax_value_pred_lm)}, ignore_index=True)
+    
+    return metric_df
+
+def lasso_lars(X_train, y_train, X_validate, y_validate, metric_df, alpha):
+    # create the model object
+    lars = LassoLars(alpha=alpha)
+    
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series!
+    lars.fit(X_train, y_train.tax_value)
+    
+    # predict train
+    y_train['tax_value_pred_lars'] = lars.predict(X_train)
+    
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.tax_value_pred_lars) ** (1/2)
+    
+    # predict validate
+    y_validate['tax_value_pred_lars'] = lars.predict(X_validate)
+    
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.tax_value_pred_lars) ** (1/2)
+    
+    metric_df = make_metric_df(y_validate.tax_value,
+               y_validate.tax_value_pred_lars,
+               'lasso_alpha_'+str(alpha),
+               metric_df)
+
+    return metric_df
+
+def tweedie(X_train, y_train, X_validate, y_validate, metric_df, power, alpha):
+    # create the model object
+    glm = TweedieRegressor(power=power, alpha=alpha)
+    
+    
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    glm.fit(X_train, y_train.tax_value)
+    
+    # predict train
+    y_train['tax_value_pred_glm'] = glm.predict(X_train)
+    
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.tax_value_pred_glm) ** (1/2)
+    
+    # predict validate
+    y_validate['tax_value_pred_glm'] = glm.predict(X_validate)
+    
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.tax_value_pred_glm) ** (1/2)
+    
+    metric_df = make_metric_df(y_validate.tax_value,
+               y_validate.tax_value_pred_glm,
+               'glm_power_'+str(power)+"_aplha_"+str(alpha),
+               metric_df)
+
+    return metric_df
+
+def poly(X_train, y_train, X_validate, y_validate, metric_df, degree):
+    # make the polynomial features to get a new set of features
+    pf = PolynomialFeatures(degree=degree)
+    
+    # fit and transform X_train_scaled
+    X_train_degree2 = pf.fit_transform(X_train)
+    
+    # transform X_validate_scaled & X_test_scaled
+    X_validate_degree2 = pf.transform(X_validate)
+    #X_test_degree2 =  pf.transform(X_test)
+    
+    # create the model object
+    lm2 = LinearRegression(normalize=True)
+    
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lm2.fit(X_train_degree2, y_train.tax_value)
+    
+    # predict train
+    y_train['tax_value_pred_lm2'] = lm2.predict(X_train_degree2)
+    
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.tax_value_pred_lm2) ** (1/2)
+    
+    # predict validate
+    y_validate['tax_value_pred_lm2'] = lm2.predict(X_validate_degree2)
+    
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.tax_value_pred_lm2) ** 0.5
+    metric_df = make_metric_df(y_validate.tax_value,
+                   y_validate.tax_value_pred_lm2,
+                   'poly_degree_'+str(degree),
+                   metric_df)
+
+    return metric_df
+
+def metric(X_train, y_train, X_validate, y_validate, metric_df):
+    
+    #baseline
+    
+    metric_df = baseline(y_train, y_validate, metric_df)
+    
+    #ols
+    metric_df = ols(X_train, y_train, X_validate, y_validate, metric_df)
+    
+    #lasso-lars
+    for i in range (2, 6):
+        metric_df = lasso_lars(X_train, y_train, X_validate, y_validate, metric_df, i)
+    
+    #tweedie
+    for power in range (0, 5):
+        for alpha in range(0, 5):
+            metric_df = tweedie(X_train, y_train, X_validate, y_validate, metric_df, power, alpha)
+    
+    #Poly
+    for degree in range(2, 6):
+        metric_df = poly(X_train, y_train, X_validate, y_validate, metric_df, degree)
+    
+    return metric_df
+
+########################### Graphing ##########################
+
+def plot_variable_pairs(train, cols, descriptive=None, hue=None):
+    '''
+    This function takes in a df, a list of cols to plot, and default hue=None 
+    and displays a pairplot with a red regression line. If passed a descriptive
+    dictionary, converts axis titles to the corresponding names.
+    '''
+    # sets line-plot options and scatter-plot options
+    keyword_arguments={'line_kws':{'color':'red'}, 'scatter_kws': {'alpha': 0.1}}
+    
+    # creates pairplot object
+    pairplot = sns.pairplot(train[cols], hue=hue, kind="reg",\
+            plot_kws=keyword_arguments)
+    
+    # if passed a descriptive dictionary, iterates through matplotlib axes
+    # in our pairplot object and sets their axis labels to the corresponding 
+    # strings.
+    if descriptive:
+        for ax in pairplot.axes.flat:
+            ax.set_xlabel(descriptive[ax.get_xlabel()])
+            ax.set_ylabel(descriptive[ax.get_ylabel()])
+    
+    # Adds a super-title
+    pairplot.fig.suptitle('Correlation of Continuous Variables', y=1.08)
+    plt.show()
+
+def create_heatmap(train, cols, descriptive=None):
+    corr_matrix = train[cols].corr()
+
+    kwargs = {'alpha':.9,'linewidth':3, 'linestyle':'-', 
+          'linecolor':'k','rasterized':False, 'edgecolor':'w', 
+          'capstyle':'projecting',}
+    labels = pd.Series(cols)
+    if descriptive:
+        labels = labels.map(descriptive)
+    plt.figure(figsize=(8,6))
+    heatmap = sns.heatmap(corr_matrix, cmap='Purples', annot=True, \
+                          xticklabels=labels, yticklabels=labels, vmin=0, vmax=1, **kwargs)
+    plt.ylim(0, 3)
+    plt.title('Correlation of Continuous Variables')
+    plt.show()
+
+def plot_categorical_and_continuous_vars(categorical_vars, continuous_vars, df, descriptive=None):
+    """
+    This function that takes in a string name of a categorical variable, 
+    a string name from a continuous variable and the df they live in and
+    displays 4 different plots.
+    """
+    for categorical_var in categorical_vars:
+        for continuous_var in continuous_vars:
+            categorical_label = categorical_var
+            continuous_label = continuous_var
+            if descriptive:
+                categorical_label = descriptive[categorical_var]
+                continuous_label = descriptive[continuous_var]
+            
+            fig, axes = plt.subplots(figsize=(12,36), nrows=4,ncols=1)
+            fig.suptitle(f'{continuous_label} by {categorical_label}', fontsize=18, y=1.02)
+            sns.lineplot(ax=axes[0], x=categorical_var, y=continuous_var, data=df)
+            axes[0].set_title('Line Plot', fontsize=14)
+            axes[0].set_xlabel(categorical_label, fontsize=12)
+            axes[0].set_ylabel(continuous_label, fontsize=12)
+            
+            sns.boxplot(ax=axes[1], x=categorical_var, y=continuous_var, data=df,\
+                        color='blue')
+            axes[1].set_title('Box-and-Whiskers Plot', fontsize=14)
+            axes[1].set_xlabel(categorical_label, fontsize=12)
+            axes[1].set_ylabel(continuous_label, fontsize=12)
+            
+            sns.swarmplot(ax=axes[2], x=categorical_var, y=continuous_var, data=df,\
+                        palette='Blues')
+            axes[2].set_title('Swarm Plot', fontsize=14)
+            axes[2].set_xlabel(categorical_label, fontsize=12)
+            axes[2].set_ylabel(continuous_label, fontsize=12)
+            
+            sns.barplot(ax=axes[3], x=categorical_var, y=continuous_var, data=df,\
+                        palette='Purples')
+            axes[3].set_title('Bar Plot', fontsize=14)
+            axes[3].set_xlabel(categorical_label, fontsize=12)
+            axes[3].set_ylabel(continuous_label, fontsize=12)
+            
+            plt.tight_layout()
+
+            plt.show()
+
